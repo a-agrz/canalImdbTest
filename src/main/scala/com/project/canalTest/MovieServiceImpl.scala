079297b4f19c15a234025d2bad7356c7c1f99d75
Abdalla Aguerzame <aaguerzame@outlook.com>
2023-06-13 01:05:41 +0200

    done implementing tvSeriesWithGreatestNumberOfEpisodes

--- src/main/scala/com/project/canalTest/MovieServiceImpl.scala
+++ src/main/scala/com/project/canalTest/MovieServiceImpl.scala
@@ -4,7 +4,7 @@ import akka.actor.ActorSystem
 import akka.stream.{ActorMaterializer, IOResult, Materializer}
 import akka.stream.alpakka.csv.scaladsl.{CsvParsing, CsvToMap}
 import akka.stream.scaladsl.{FileIO, Source}
-import com.project.canalTest.Schema.{Principal, Title, TitleActor}
+import com.project.canalTest.Schema.{Principal, Title, TitleActor, TvSeries}
 
 import java.nio.file.Paths
 import scala.concurrent.Future
@@ -17,13 +17,15 @@ trait MovieService {
 }
 
 object MovieServiceImpl extends MovieService {
+
   implicit val system = ActorSystem("MovieServiceSpec")
   implicit val materializer = ActorMaterializer
+  val nameBasicsFile = "src/main/ressources/name.basics.test.tsv" //"src/main/ressources/name.basics.tsv"
+  val titleBasicsFile = "src/main/ressources/title.basics.test.tsv" //"src/main/ressources/title.basics.tsv"
+  val titlePrincipalsFile = "src/main/ressources/title.principals.test.tsv" //"src/main/ressources/title.principals.tsv"
+  val titleEpisodesFile = "src/main/ressources/title.principals.test.tsv" //"src/main/ressources/title.principals.tsv"
 
   override def principalsForMovieName(name: String): Source[Schema.Principal, _] = {
-    val nameBasicsFile = "src/main/ressources/name.basics.test.tsv" //"src/main/ressources/name.basics.tsv"
-    val titleBasicsFile = "src/main/ressources/title.basics.test.tsv" //"src/main/ressources/title.basics.tsv"
-    val titlePrincipalsFile = "src/main/ressources/title.principals.test.tsv" //"src/main/ressources/title.principals.tsv"
 
     lazy val titleActor = tsvSource(titlePrincipalsFile)
       .map(x =>
@@ -44,7 +46,7 @@ object MovieServiceImpl extends MovieService {
             x.getOrElse("primaryProfession", "").split(",").toList
           ))
 
-    lazy val title = tsvSource(titleBasicsFile, Some(TsvFilter("primaryTitle", name)))
+    lazy val title: Source[Title, _] = tsvSource(titleBasicsFile, Some(TsvFilter("primaryTitle", name)))
       .map(x =>
         Title(
           x.getOrElse("tconst", ""),
@@ -53,11 +55,11 @@ object MovieServiceImpl extends MovieService {
 
     val principalsFortitle = title.flatMapConcat(title =>
       titleActor.collect {
-        case titleActor if titleActor.tConst == title.tConst => titleActor
+        case titleActor if titleActor.tConst.toUpperCase() == title.tConst.toUpperCase() => titleActor
       })
       .flatMapConcat(actorTitle =>
         principals.collect {
-          case principal if principal.nconst == actorTitle.nConst => principal
+          case principal if principal.nconst.toUpperCase() == actorTitle.nConst.toUpperCase() => principal
         })
     principalsFortitle
   }
@@ -73,7 +75,34 @@ object MovieServiceImpl extends MovieService {
       )
   }
 
-  override def tvSeriesWithGreatestNumberOfEpisodes(): Source[Schema.TvSeries, _] = ???
+  override def tvSeriesWithGreatestNumberOfEpisodes(): Source[TvSeries, _] = {
+    lazy val tvSeries: Source[TvSeries, _] = tsvSource(titleBasicsFile)
+      .filter(x => x.getOrElse("titleType","") == "tvseries")
+      .map(x =>
+        TvSeries(
+          x.getOrElse("tconst", ""),
+          x.getOrElse("original", ""),
+          x.get("startYear").flatMap(x => Try(x.toInt).toOption),
+          x.get("endYear").flatMap(x => Try(x.toInt).toOption),
+          x.getOrElse("genre", "").split(",").toList
+        ))
+
+    tsvSource(titleEpisodesFile)
+      .map(x => x.getOrElse("parentTcount", "") -> 1)
+      .groupBy(8, x => x._1)
+      .reduce((x, y) => (x._1, x._2 + y._2))
+      .mergeSubstreams
+      .fold(Seq.empty[(String, Int)]) {
+        case (acc, e) if acc.length < 10 => acc ++ Seq(e)
+        case (acc, e) if !acc.filter(_._2 < e._2).isEmpty => acc.sortBy(_._2).tail ++ Seq(e)
+        case (acc, e) => acc
+      }
+      .flatMapConcat(episode =>
+        tvSeries.collect {
+          case tvSerie if (!episode.filter(_._1.toUpperCase() == tvSerie.tconst.toUpperCase()).isEmpty) => tvSerie
+        })
+
+  }
 
   case class TsvFilter(column: String, value: String)
 }
